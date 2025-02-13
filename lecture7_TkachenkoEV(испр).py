87e9b2974844a1a4d9909fd5d41d3c38e6ea87ea
MarcusTesla <93238936+MarcusTesla@users.noreply.github.com>
2025-02-13 23:36:54 +0500

    Add files via upload

---
+++ lecture7_TkachenkoEV(испр).py
@@ -0,0 +1,32 @@
+#Необходимо дополнить "Практическое задание №6" таким образом, чтобы в конце программы мы вызвали статический метод родительского класса Animal, который вывел бы нам количество всех созданных экземпляров.
+#Если мы создали трех наследников в предыдущем задании, то наш метод должен вывести на экран число 3.
+
+class Animal:
+    numInstances = 0
+    def __init__(self):
+        Animal.numInstances+=1
+
+    @staticmethod
+    def get_numInstances():
+        return Animal.numInstances
+
+    def voice(self):
+        pass
+
+class Cat(Animal):
+    def voice(self, sound='meow'):
+        print(sound)
+
+class Dog(Animal):
+    def voice(self, sound='woof'):
+        print(sound)
+
+class Cow(Animal):
+    def voice(self, sound='moo'):
+        print(sound)
+
+kitty = Cat()
+tuzik = Dog()
+murka = Cow()
+
+print(Animal.get_numInstances())
